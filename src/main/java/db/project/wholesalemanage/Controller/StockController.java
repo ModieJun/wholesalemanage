9b0423e2eb544cdce47848aa2da72c1ee426b9ba
modiejun <junjie981208@gmail.com>
2019-05-29 23:55:48 +0800

    Added stock controller

---
+++ src/main/java/db/project/wholesalemanage/Controller/StockController.java
@@ -0,0 +1,39 @@
+package db.project.wholesalemanage.Controller;
+
+import db.project.wholesalemanage.Model.Stock;
+import db.project.wholesalemanage.Service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StockController {
+
+    @Autowired
+    private StockService stockService;
+
+    @GetMapping("/stock")
+    public String homeStock(Model model) {
+        model.addAttribute("stocks",stockService.getAll());
+        return "stocks";
+    }
+
+    @PostMapping("/stock/add")
+    public String addStock(@ModelAttribute Stock stock) {
+        if (stockService.addNewStock(stock)) {
+            return "success";
+        }
+        return "failure";
+    }
+
+    @GetMapping("/stock/emergency")
+    public String getEmergency(Model model) {
+        model.addAttribute("stocks",stockService.getEmergencyStocks());
+        return "stocks";
+    }
+
+
+}
