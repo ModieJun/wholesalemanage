c87d4ef345979ae263dcf6f85df8372bf9b188ef
modiejun <junjie981208@gmail.com>
2019-05-29 17:38:27 +0800

    Added find by name (Stockrepo)

--- src/main/java/db/project/wholesalemanage/Database/StockRepo.java
+++ src/main/java/db/project/wholesalemanage/Database/StockRepo.java
@@ -4,4 +4,5 @@ import db.project.wholesalemanage.Model.Stock;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StockRepo extends CrudRepository<Stock,Long> {
+    Stock findByName(String name);
 }
