1c946fb2621cce9714696c6d8564fd5637c4a832
modiejun <junjie981208@gmail.com>
2019-05-29 17:31:26 +0800

    Adde SupplierService Implementation

---
+++ src/main/java/db/project/wholesalemanage/Service/SupplierServiceImpl.java
@@ -0,0 +1,31 @@
+package db.project.wholesalemanage.Service;
+
+import db.project.wholesalemanage.Database.SupplierRepo;
+import db.project.wholesalemanage.Model.Supplier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SupplierServiceImpl implements SupplierService {
+
+    @Autowired
+    private SupplierRepo supplierRepo;
+
+    @Override
+    public Iterable<Supplier> getAll() {
+        return supplierRepo.findAll();
+    }
+
+    @Override
+    public Boolean addNewSupplier(Supplier supplier) {
+        Supplier supp= supplierRepo.findByName(supplier.getName());
+
+        if(supp!=null){
+            return false;
+        }
+
+        supplierRepo.save(supplier);
+        return true;
+
+    }
+}
