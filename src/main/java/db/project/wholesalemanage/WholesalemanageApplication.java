713b05ddff5726b2cf657cf9c7f387520f2a0364
modiejun <junjie981208@gmail.com>
2019-05-29 11:54:02 +0800

    Initial Commit

---
+++ src/main/java/db/project/wholesalemanage/WholesalemanageApplication.java
@@ -0,0 +1,13 @@
+package db.project.wholesalemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WholesalemanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WholesalemanageApplication.class, args);
+    }
+
+}
