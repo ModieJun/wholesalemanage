d35c4206651c721462392bdc3bd3d54ccde53677
modiejun <junjie981208@gmail.com>
2019-05-29 17:39:38 +0800

    Added StockService Impl

---
+++ src/main/java/db/project/wholesalemanage/Service/StockServiceImpl.java
@@ -0,0 +1,38 @@
+package db.project.wholesalemanage.Service;
+
+import db.project.wholesalemanage.Database.StockRepo;
+import db.project.wholesalemanage.Model.Stock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockServiceImpl implements StockService {
+
+    @Autowired
+    private StockRepo stockRepo;
+
+    @Override
+    public Boolean addNewStock(Stock stock) {
+        if (stockRepo.findByName(stock.getName())!=null) {
+            return false;
+        }
+
+        stockRepo.save(stock);
+        return true;
+    }
+
+    @Override
+    public void UpdateStock(Stock stock) {
+
+    }
+
+    @Override
+    public Iterable<Stock> getEmergencyStocks() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Stock> getAll() {
+        return stockRepo.findAll();
+    }
+}
