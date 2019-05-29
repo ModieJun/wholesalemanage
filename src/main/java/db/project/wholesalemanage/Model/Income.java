1ee0d16c523d90d0805983166e564e9715a45532
modiejun <junjie981208@gmail.com>
2019-05-29 15:57:46 +0800

    Add Getter Setter Methods (+ Data attribute)

--- src/main/java/db/project/wholesalemanage/Model/Income.java
+++ src/main/java/db/project/wholesalemanage/Model/Income.java
@@ -1,6 +1,7 @@
 package db.project.wholesalemanage.Model;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
 public class Income {
@@ -19,4 +20,61 @@ public class Income {
 
     private Long quantity;
 
+    private Date date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Long getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(Long stockId) {
+        this.stockId = stockId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
