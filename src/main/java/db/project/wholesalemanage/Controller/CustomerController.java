aa93794d0f9aff72ed696a706851533c4eb4c7fd
modiejun <junjie981208@gmail.com>
2019-05-29 23:31:50 +0800

    Added customer controller with get and post

---
+++ src/main/java/db/project/wholesalemanage/Controller/CustomerController.java
@@ -0,0 +1,37 @@
+package db.project.wholesalemanage.Controller;
+
+import db.project.wholesalemanage.Model.Customer;
+import db.project.wholesalemanage.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customer")
+    private String home() {
+        return "customer";
+    }
+
+
+    /*
+    *       Post mappings
+    *
+    *
+     */
+
+
+    @PostMapping("/customer/add")
+    private String addCustomer(@ModelAttribute Customer customer) {
+        if (customerService.addNewCustomer(customer)) {
+            return "failure";
+        }
+        return "success";
+    }
+}
