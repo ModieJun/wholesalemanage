2fcfa68702d313c311d8f14846e7e497c045595e
modiejun <junjie981208@gmail.com>
2019-05-30 09:42:14 +0800

    added two repo query methods

--- src/main/java/db/project/wholesalemanage/Database/IncomeRepo.java
+++ src/main/java/db/project/wholesalemanage/Database/IncomeRepo.java
@@ -1,7 +1,10 @@
 package db.project.wholesalemanage.Database;
 
 import db.project.wholesalemanage.Model.Income;
+import db.project.wholesalemanage.Model.Type;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IncomeRepo extends CrudRepository<Income,Long> {
+    Iterable<Income> findByType(Type type);
+    Iterable<Income> findByDateContaining(Long month);
 }
